7429665b86c30918ff7fb4a4297088c8f20bd4d4
Romach33 <d.romach33@mail.ru>
2020-12-15 18:41:20 +0300

    task3

---
+++ src/com/company/number2/Square4.java
@@ -0,0 +1,23 @@
+package com.company.number2;
+
+public class Square4 {
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
+
+    public Square4(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    /**
+     * Проверяет, находится ли точка (x, y) Внутри квадрата
+     */
+    public boolean isPointAboveSquare(double x, double y) {
+
+        return ((x > x1) && (x < x2) && (y < y1) && (y > y2));
+
+    }
+}
